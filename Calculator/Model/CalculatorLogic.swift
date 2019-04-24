f4a292f0e1ae1dae1cf159e7c4ae06429cdb447f
Isaac Iniongun <iniongunisaac@gmail.com>
2019-04-24 17:54:37 +0100

    Finish Calculator

---
+++ Calculator/Model/CalculatorLogic.swift
@@ -0,0 +1,62 @@
+//
+//  CalculatorLogic.swift
+//  Calculator
+//
+//  Created by Isaac Iniongun on 23/04/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CalculatorLogic {
+    
+    private var number: Double?
+    
+    private var intermediateCalculation: (n1: Double, calcMethod: String)?
+    
+    mutating func setNumber(_ number: Double){
+        self.number = number
+    }
+    
+    mutating func calculate(symbol: String) -> Double? {
+        
+        if let n = number {
+            if symbol == "+/-" {
+                return n * -1
+            } else if symbol == "AC" {
+                return 0
+            } else if symbol == "%" {
+                return n * 0.01
+            } else if symbol == "=" {
+                return performTwoNumCalculation(n2: n)
+            } else {
+                intermediateCalculation = (n1: n, calcMethod: symbol)
+            }
+        }
+        
+        return nil
+    }
+    
+    private func performTwoNumCalculation(n2: Double) -> Double? {
+        
+        if let n1 = intermediateCalculation?.n1, let operation = intermediateCalculation?.calcMethod {
+            
+            switch operation {
+            case "+":
+                    return n1 + n2
+            case "-":
+                return n1 - n2
+            case "×":
+                return n1 * n2
+            case "÷":
+                return n1 / n2
+            default:
+                fatalError("Operation doesn't exist.")
+            }
+            
+        }
+        
+        return nil
+    }
+    
+}
